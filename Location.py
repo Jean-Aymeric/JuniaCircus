46fa11f3c74abf533c7621a396e6e6aa1983155b
antimepix <gildasguyomard03@gmail.com>
2022-09-26 12:04:37 +0200

    fini

--- Location.py
+++ Location.py
@@ -1,2 +1,18 @@
 class Location:
-    ...
+import Show from Show
+
+    __name : str
+    __shows : [Show]
+
+    def __init__(self, name: str) :
+        self.__name = name
+        self.__shows:[]
+
+
+    def getName(self) -> str:
+        return self.__name
+
+
+    def addShows(self, show:Show ):
+        if show not in self.__shows:
+            self.__shows.append(show)

64caeb2e55f29310f5a6fd5d26ed4441898ce3f9
margauxfouque <114390471+margauxfouque@users.noreply.github.com>
2022-09-26 11:57:02 +0200

    Modification : implémentation de Artist et Team (#3)
    Co-authored-by: Margaux <fouquemargaux02@gmail.com>

--- Artist.py
+++ Artist.py
@@ -1,2 +1,28 @@
+from Performance import Performance
+
 class Artist:
-    ...
+    __name: str
+    __performances: Performance
+    __teams : []
+
+    def __init__(self, name: str, performance: Performance):
+        self.__name = name
+        self.__performances = []
+        self.__teams = []
+
+    def getName(self) -> str :
+        return(self.__name)
+
+    def perform(self) -> str :
+        return (self.__performances)
+
+    def addPerformance(self, performance: Performance):
+        if performance not in self.__performances:
+            self.__performances.append(performance)
+            performance.addArtist(self)
+
+    def addTeam(self, team):
+        if team not in self.__teams :
+            self.teams.append(team)
+            team.addArtist(self)
+

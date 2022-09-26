ba69d6ac29083336bb9ba0fd810e6e07f1d5601e
Clem42000 <114388720+Clem42000@users.noreply.github.com>
2022-09-26 12:24:38 +0200

    Implémentation de Show (#2)
    * Implémentation de Show
    
    * Implémentation de Show
    Modification du date
    
    * Implémentation de Show
    Modification du date
    modif datetime pour hours
    
    * Implémentation de Show
    Modification du date
    modif datetime pour hours
    modi du str

--- Show.py
+++ Show.py
@@ -1,2 +1,37 @@
+from Artist import Artist
+from Performance import Performance
+import Datetime
 class Show:
-    ...
+
+    __date : date
+    __hour : datetime
+    __name : str
+    __location: []
+    __artist: [Artist]
+    __performance: [Performance]
+
+    def __init__(self,name: str, date : date , hour: datetime):
+            self.__date = date
+            self.__name = name
+            self.__hour = hour
+            self.__artist = []
+            self.__performance = []
+
+        def getName(self):
+            return self.__name
+
+        def getDate(self):
+            return self.__date
+
+        def getHour(self):
+            return self.__hour
+
+        def getLocation(self):
+            return self.__location
+
+        def addPerformance(self, performance: Performance):
+            self.__performance.append(performance)
+
+        def addArtist(self, artist: Artist):
+            self.__artist.append(artist)
+            #j'ai bcp trop taffer sur ce projet!!!  (qui peux me test?)

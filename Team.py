b36524adeb40bf413d5462aaf49b123a3337604b
Margaux <fouquemargaux02@gmail.com>
2022-09-26 11:49:47 +0200

    Modification : implémentation de Artist et Team

--- Team.py
+++ Team.py
@@ -1,2 +1,15 @@
+from Artist import Artist
+
 class Team:
-    ...
+    __name : str
+    __artists : []
+
+    def __init__(self, name: str):
+        self.__name = name
+        self.__artists = []
+
+    def getName(self) -> str :
+        return(self.__name)
+
+    def addArtist(self, __artist : Artist):
+        self.__artist.append(Artist)
